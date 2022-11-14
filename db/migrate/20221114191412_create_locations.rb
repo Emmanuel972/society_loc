5116728245561262dfef984789b184c1b3fd0958
Emmanuel CLAIRE <c.l.e.972@gmail.com>
2022-11-14 20:20:16 +0100

    add game and location models

---
+++ db/migrate/20221114191412_create_locations.rb
@@ -0,0 +1,13 @@
+class CreateLocations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :locations do |t|
+      t.string :name
+      t.date :start
+      t.date :end
+      t.references :game, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_14_180704) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_14_191412) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "games", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "locations", force: :cascade do |t|
+    t.string "name"
+    t.date "start"
+    t.date "end"
+    t.bigint "game_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_locations_on_game_id"
+    t.index ["user_id"], name: "index_locations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +48,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_14_180704) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "locations", "games"
+  add_foreign_key "locations", "users"
 end
