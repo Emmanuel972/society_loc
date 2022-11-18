d9c9102da784b4a8990d1677c900d784277f0644
Emmanuel CLAIRE <c.l.e.972@gmail.com>
2022-11-18 16:59:51 +0100

    add cloudinary to user and navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,28 +4,32 @@ require "open-uri"
 #
 # Examples:
 #
+Location.destroy_all
+Game.destroy_all
+User.destroy_all
+
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 User.create!(email: 'test@test.com', password: '123456')
 # db/seeds.rb
-puts "Cleaning database..."
-Game.destroy_all
+# puts "Cleaning database..."
+# Game.destroy_all
 
-puts "Creating games..."
+# puts "Creating games..."
 
-bios = {name: "Bios :Mesofouna ", description: "Bios : Mesofauna fait partie de la série de jeux Bios. Il s'agit d'une version plus accessible de Bios : Megafau..."}
-insolito = {name: "Insolito", description: "Constituez deux équipes de choc et affrontez-vous à base de questions aussi improbables que farfelues."}
-old = {name: "Old london ridge", description: "Old London Bridge est un jeu de placement d'ouvrier dans lequel vous devez reconstruire le pont de Londr..."}
-punto = {name: "Punto", description: "Punto est un jeu de cartes dans lequel les joueurs doivent poser 4 cartes de la même couleur dans le bu..."}
-las_vegas = {name: "Las Vegas", description: "Dans Las Vegas, placez astucieusement vos dés pour essayer de remporter un maximum de cash sous le n..."}
-vilainous = {name: "Vilaious", description: "Villainous est un jeu de société qui vous met dans la peau de méchants emblématiques, bien décidés à re..."}
-tres_fute = {name: "Trés futé", description: "Très futé est un jeu de dés, de combinaisons et de prise de risque. Lancez les dés et cochez les bonnes..."}
-esquisse = {name: "Esquissé ?", description: "Esquissé ? est LE jeu d’ambiance par excellence. Basé sur le principe du téléphone arabe, le jeu trouve son o..."}
-gold = {name: "Gold", description: "Dans ce jeu de mémoire amusant, vous vous glissez dans la peau de chercheurs d’or et tentez d’en récup..."}
-control = {name: "Control", description: "Control est un jeu de cartes dans lequel les joueurs incarnent des voyageurs dans le temps qui ont été pri..."}
+# bios = {name: "Bios :Mesofouna ", description: "Bios : Mesofauna fait partie de la série de jeux Bios. Il s'agit d'une version plus accessible de Bios : Megafau..."}
+# insolito = {name: "Insolito", description: "Constituez deux équipes de choc et affrontez-vous à base de questions aussi improbables que farfelues."}
+# old = {name: "Old london ridge", description: "Old London Bridge est un jeu de placement d'ouvrier dans lequel vous devez reconstruire le pont de Londr..."}
+# punto = {name: "Punto", description: "Punto est un jeu de cartes dans lequel les joueurs doivent poser 4 cartes de la même couleur dans le bu..."}
+# las_vegas = {name: "Las Vegas", description: "Dans Las Vegas, placez astucieusement vos dés pour essayer de remporter un maximum de cash sous le n..."}
+# vilainous = {name: "Vilaious", description: "Villainous est un jeu de société qui vous met dans la peau de méchants emblématiques, bien décidés à re..."}
+# tres_fute = {name: "Trés futé", description: "Très futé est un jeu de dés, de combinaisons et de prise de risque. Lancez les dés et cochez les bonnes..."}
+# esquisse = {name: "Esquissé ?", description: "Esquissé ? est LE jeu d’ambiance par excellence. Basé sur le principe du téléphone arabe, le jeu trouve son o..."}
+# gold = {name: "Gold", description: "Dans ce jeu de mémoire amusant, vous vous glissez dans la peau de chercheurs d’or et tentez d’en récup..."}
+# control = {name: "Control", description: "Control est un jeu de cartes dans lequel les joueurs incarnent des voyageurs dans le temps qui ont été pri..."}
 
-[bios, insolito, old, punto, las_vegas, vilainous, tres_fute, esquisse, gold, control].each do |attributes|
-  game = Game.create!(attributes)
-  puts "Created #{game.name}"
-end
-puts "Finished!"
+# [bios, insolito, old, punto, las_vegas, vilainous, tres_fute, esquisse, gold, control].each do |attributes|
+#   game = Game.create!(attributes)
+#   puts "Created #{game.name}"
+# end
+# puts "Finished!"
